b80a83a5be1a27e27c78ea27635245aa7b1b4375
SIDDHI - <siddhi.23@abdn.ac.uk>
2024-02-23 11:39:17 +0000

    Adding all the files created for polar_bears practical

--- parse_csv.py
+++ parse_csv.py
@@ -12,6 +12,12 @@ print("table dropped successfully");
 conn.execute('CREATE TABLE deployments (BearID INTEGER, PTT_ID INTEGER, capture_lat REAL, capture_long REAL, Sex TEXT, Age_class TEXT, Ear_applied TEXT)')
 print("table created successfully");
 
+conn.execute('DROP TABLE IF EXISTS status')
+print("table dropped successfully");
+# create table again
+conn.execute('CREATE TABLE status (status_id INTEGER PRIMARY KEY AUTOINCREMENT, deployID INTEGRER, received TEXT, latitude REAL, longitude REAL, temperature REAL, deployment_id INTEGER)')
+print("table created successfully");
+
 # open the file to read it into the database
 with open('PolarBear_Telemetry_southernBeaufortSea_2009_2011/USGS_WC_eartag_deployments_2009-2011.csv', newline='') as f:
     reader = csv.reader(f, delimiter=",")
@@ -30,6 +36,28 @@ with open('PolarBear_Telemetry_southernBeaufortSea_2009_2011/USGS_WC_eartag_depl
         cur.execute('INSERT INTO deployments VALUES (?,?,?,?,?,?,?)', (BearID, PTT_ID, capture_lat, capture_long, Sex, Age_class, Ear_applied))
         conn.commit()
 print("data parsed successfully");
+
+# open the file to read it into the database
+with open('PolarBear_Telemetry_southernBeaufortSea_2009_2011/USGS_WC_eartags_output_files_2009-2011-Status.csv', newline='') as f:
+    reader = csv.reader(f, delimiter=",")
+    next(reader) # skip the header line
+    for row in reader:
+        print(row)
+        if(row[1] == ''):
+          continue
+
+        pttID = int(row[1])
+        received = row[2]
+        latitude = row[4]
+        longitude = row[5]
+        temperature = row[9]
+        deploymentID = row[0]
+
+        cur.execute('INSERT INTO status VALUES (NULL,?,?,?,?,?,?)', (pttID, received, latitude, longitude, temperature, deploymentID))
+        conn.commit()
+print("data parsed successfully");
+
+
 
 conn.close()
         
--- polar_bears.py
+++ polar_bears.py
@@ -13,3 +13,15 @@ def index():
     rows = cur.fetchall()
     conn.close()
     return render_template('index.html', rows=rows)
+
+
+@app.route('/<id>')
+def deployment(id):
+    # open the connection to the database
+    conn = sqlite3.connect('polar_bear_data.db')
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute("select * from status WHERE deployment_id="+ id +" AND received<>''" +" AND latitude<>''" +" AND longitude<>''" +" AND temperature<>''")
+    rows = cur.fetchall()
+    conn.close()
+    return render_template('deployment.html', rows=rows)
